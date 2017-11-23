4e6d6c3563df3fdc13974132117064e7fde5d369
Cristiano Betta <cristiano@betta.io>
2017-11-23 15:46:20 +0000

    Add shopping APIs

--- lib/amadeus/client/shopping/flight_offers.rb
+++ lib/amadeus/client/shopping/flight_offers.rb
@@ -10,6 +10,24 @@ module Amadeus
       #   amadeus.shopping.flight_offers
       #
       class FlightOffers < Amadeus::Client::Decorator
+        # Find the cheapest bookable flights.
+        #
+        # @option params [String] :origin City/Airport IATA code from which the
+        #   flight will depart. BOS, for example.
+        # @option params [String] :destination City/Airport IATA code to which
+        #   the traveler is going. PAR, for example
+        # @return [Amadeus::Response] a parsed response
+        # @raise [Amadeus::Exceptions::Base] an exception if the call failed
+        # @example Find the cheapest destination from London to Paris for Xmas
+        #   amadeus.shopping.flight_offers.get(
+        #     origin: 'LHR',
+        #     destination: 'LAX',
+        #     departureDate: '2017-12-24'
+        #   )
+        #
+        def get(params = {})
+          client.get('/v1/shopping/flight-offers', params)
+        end
       end
     end
   end
