bd62755b8860eee6d89206b36deaa70af33aaf3b
Cristiano Betta <cristiano@betta.io>
2017-11-23 16:54:13 +0000

    Add travel/hotels" endpoints

--- lib/amadeus/client/shopping/hotel_offers.rb
+++ lib/amadeus/client/shopping/hotel_offers.rb
@@ -10,6 +10,25 @@ module Amadeus
       #   amadeus.shopping.hotel_offers
       #
       class HotelOffers < Amadeus::Client::Decorator
+        # Search for hotels and retrieve availability and rates information
+        #
+        # @option params [String] :cityCode City IATA code
+        # @option params [Double] :latitude latitude of geographic location to
+        #   search around. Example: 52.5238
+        # @option params [Double] :longitude Longitude of geographic location to
+        #   search around. Example: 13.3835
+        # @option params [String] :hotels Comma separated list of Amadeus hotel
+        #   codes to request. Example: RTPAR001
+        # @return [Amadeus::Response] a parsed response
+        # @raise [Amadeus::Exceptions::Base] an exception if the call failed
+        # @example Search for hotels in London
+        #   amadeus.shopping.hotel_offers.get(
+        #     origin: 'LHR'
+        #   )
+        #
+        def get(params = {})
+          client.get('/v1/shopping/hotel-offers', params)
+        end
       end
     end
   end
